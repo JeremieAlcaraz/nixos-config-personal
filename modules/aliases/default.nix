cc36d93a9f7eec8b1efadbd330e158fc3122134a
JeremieAlcaraz <hello@jeremiealcaraz.com>
2025-08-15 19:20:07 +0200

    update aliases to rebuild flake

--- modules/aliases/default.nix
+++ modules/aliases/default.nix
@@ -22,16 +22,20 @@
       lg = "lazygit";
 
       # Nix
-      rebuild = "nix flake lock --update-input neovim && sudo nixos-rebuild switch --flake .#nixos";
+      # Mets à jour SEULEMENT l'input neovim du flake AU BON CHEMIN,
+      # puis rebuild NixOS pour l'host "nixos" (adapte si ton host s'appelle autrement)
+      rebuild = "nix flake lock --update-input neovim --flake $HOME/nix-config && sudo nixos-rebuild switch --flake $HOME/nix-config#nixos";
 
+      # (optionnel) tout mettre à jour
+      rebuild-all = "nix flake update --flake $HOME/nix-config && sudo nixos-rebuild switch --flake $HOME/nix-config#nixos";
+      rebuild-test = "sudo nixos-rebuild test   --flake ~/nix-config#nixos";
+      rebuild-boot = "sudo nixos-rebuild boot   --flake ~/nix-config#nixos";
       # Divers
       v = "nvim";
       ls = "eza --group-directories-first --icons";
       ll = "eza -l --git";
       la = "eza -la --git";
       # rebuild = "sudo nixos-rebuild switch --flake ~/nix-config#nixos";
-      rebuild-test = "sudo nixos-rebuild test   --flake ~/nix-config#nixos";
-      rebuild-boot = "sudo nixos-rebuild boot   --flake ~/nix-config#nixos";
     };
   };
 }
