6558232c6a5ef41ff5205f78b4d3e967132de9bd
JeremieAlcaraz <hello@jeremiealcaraz.com>
2025-10-02 09:23:43 +0200

    add linger auto but vscode server not work

--- flake.nix
+++ flake.nix
@@ -23,68 +23,70 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, vicinae, ... }@inputs:
-    let
-      mkFormatter = system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        pkgs.writeShellApplication {
-          name = "fmt";
-          runtimeInputs = [ pkgs.nixpkgs-fmt pkgs.findutils ];
-          text = builtins.readFile ./scripts/fmt.sh;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    vicinae,
+    ...
+  } @ inputs: let
+    mkFormatter = system: let
+      pkgs = nixpkgs.legacyPackages.${system};
     in
-    rec {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./host/nixos/configuration.nix
-
-            # Module VS Code Server (recommandé)
-            inputs.vscode-server.nixosModules.default
-            ({ ... }: {
-              services.vscode-server = {
-                enable = true;
-
-                # Surveille les deux emplacements utilisés par VS Code
-                installPath = [
-                  "$HOME/.vscode-server"                # ancien chemin (bin/<commit>/)
-                  "$HOME/.vscode-server/cli/servers"    # nouveau chemin (cli/servers/Stable-*/server/)
-                ];
-
-                # Optionnel: si certaines extensions exigent un env FHS
-                # enableFHS = true;
-              };
-
-              # LINGERING déclaratif: systemd --user actif au boot pour 'jeremie'
-              services.logind.lingerUsers = [ "jeremie" ];
-
-              # (Optionnel) compat binaire générique via nix-ld :
-              # programs.nix-ld.enable = true;
-            })
-
-            # module Home-Manager
-            home-manager.nixosModules.home-manager
-
-            # glue pour charger la config user
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # Ajoutez cette ligne pour passer inputs
-              home-manager.extraSpecialArgs = { inherit inputs; };
-
-              home-manager.users.jeremie = import ./home-manager/home.nix;
-
-            }
-          ];
-        };
+      pkgs.writeShellApplication {
+        name = "fmt";
+        runtimeInputs = [pkgs.nixpkgs-fmt pkgs.findutils];
+        text = builtins.readFile ./scripts/fmt.sh;
       };
+  in rec {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./host/nixos/configuration.nix
 
-      formatter = {
-        x86_64-linux = mkFormatter "x86_64-linux";
-        aarch64-darwin = mkFormatter "aarch64-darwin";
+          # Module VS Code Server (recommandé)
+          inputs.vscode-server.nixosModules.default
+          ({...}: {
+            services.vscode-server = {
+              enable = true;
+
+              # Surveille les deux emplacements utilisés par VS Code
+              installPath = [
+                "$HOME/.vscode-server" # ancien chemin (bin/<commit>/)
+                "$HOME/.vscode-server/cli/servers" # nouveau chemin (cli/servers/Stable-*/server/)
+              ];
+
+              # Optionnel: si certaines extensions exigent un env FHS
+              # enableFHS = true;
+            };
+
+            # LINGERING déclaratif: systemd --user actif au boot pour 'jeremie'
+            users.users.jeremie.linger = true;
+
+            # (Optionnel) compat binaire générique via nix-ld :
+            # programs.nix-ld.enable = true;
+          })
+
+          # module Home-Manager
+          home-manager.nixosModules.home-manager
+
+          # glue pour charger la config user
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # Ajoutez cette ligne pour passer inputs
+            home-manager.extraSpecialArgs = {inherit inputs;};
+
+            home-manager.users.jeremie = import ./home-manager/home.nix;
+          }
+        ];
       };
     };
+
+    formatter = {
+      x86_64-linux = mkFormatter "x86_64-linux";
+      aarch64-darwin = mkFormatter "aarch64-darwin";
+    };
+  };
 }
